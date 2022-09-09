35c67b58fc24fb2aa82816d984ea4d466d107a5d
Tristan Bower <45320180+hungry27@users.noreply.github.com>
2022-09-09 16:45:46 -0400

    Update DCPupper.js

--- DCPupper.js
+++ DCPupper.js
@@ -1,1 +1,12 @@
+async function main (){
+  const compute = require('dcp/compute');
+    // Rest of the code will go in the following sections:
+  /* INPUT DATA */
 
+  /* WORK FUNCTION */
+
+  /* COMPUTE FOR */
+
+  /* PROCESS RESULTS */
+}
+require('dcp-client').init('https://scheduler.distributed.computer').then(main);
